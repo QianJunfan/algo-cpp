0ca26495312d1c5d3b4413cd72f7efb1b6eb281b
Qian_Junfan <qianjunfan0@gmail.com>
2026-01-18 09:15:57 +0900

    Jan 18, 2026
    add 1520d.cpp

---
+++ 1520d.cpp
@@ -0,0 +1,44 @@
+/* codeforces 1520d - same differences
+ * jan 18, 2026
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define _yes cout << "yes" << "\n"
+#define _no  cout << "no"  << "\n"
+
+void solve(void)
+{
+        int n;
+        cin >> n;
+
+        map<int, ll> c;
+        ll a = 0;
+
+        for (int i = 0; i < n; ++i) {
+                int b;
+                cin >> b;
+                int t = b - i;
+
+                a += c[t];
+                c[t]++;
+        }
+        
+        cout << a << "\n";
+}
+
+int main(void)
+{
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+ 
+        int t;
+        cin >> t;
+        
+        while (t--)
+                solve();
+
+        return 0;
+}
